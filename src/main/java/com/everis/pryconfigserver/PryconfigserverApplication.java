c89facb2ad6119b1a68b789cfaec1126996418fe
Luis Hernan Mendez Sanchez <lmendesa@everis.com>
2019-11-07 17:55:48 -0500

    pryconfigserver

---
+++ src/main/java/com/everis/pryconfigserver/PryconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.everis.pryconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PryconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryconfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/pryconfigserver/PryconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.pryconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
